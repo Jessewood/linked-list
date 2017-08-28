cad84b7a3cf8ed98e8bc64334463409c1bf6d1e4
Brandon Farestad-Rittel <bfarestadrittel@gmail.com>
2017-08-28 14:26:29 -0600

    added function to take value from inputs

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-var markRead = $('.read');
-var removeBookmark = $('.delete');
-var enterBookmark = $('.enter');
-var linkTitle = $('.link-title');
-var linkUrl = $('link-url');
-var websiteTitle1 = $('website-title-1');
-var websiteTitle2 = $('website-title-2');
-var websiteTitle3 = $('website-title-3');
-var websiteTitle4 = $('website-title-4');
-var websiteUrl1 = $('website-url-1');
-var websiteUrl2 = $('website-url-2');
-var websiteUrl3 = $('website-url-3');
-var websiteUrl4 = $('website-url-4');
 
 
-Hi brandon
+
+$('.enter').click(function() {
+	var titleInput = $('.title-input').val()
+	var urlInput = $('.url-input').val()
+	addTitle(titleInput, urlInput)
+})
+
+function addTitle(title, url){
+	//take the values of the input fields assign those to bookmark list section.
+	console.log(title, url)
+}
