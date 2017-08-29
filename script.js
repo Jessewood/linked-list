aaeeccaebb41e345cc10152ba85050d5d4214552
Brandon Farestad-Rittel <bfarestadrittel@gmail.com>
2017-08-28 20:52:57 -0600

    a-z all the css, added extra read-styling class for button

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ $('.enter').click(function() {
 	var urlInput = $('.url-input').val()
 
 	$('.bookmark-list').append(
-`<article class="card">
+	`<article class="card">
 			<h3 class="website-title website-title-1">  ${titleInput} </h3>
-			<p class="website-url website-url-1"><a href="websiteurl.com">  ${urlInput}  </a></p>
-			<button class="read">Read</button><button class="delete">Delete</button>
+			<p class="website-url website-url-1"><a href="${urlInput}">  ${urlInput}  </a></p>
+			<button class="read read-style">Read</button><button class="delete">Delete</button>
 		</article>`
 		);
 })
