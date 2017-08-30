7a50fb54643475d5e45a9d52a527af7cf8a804f1
Jesse Packwood <packwoodjustin@gmail.com>
2017-08-30 12:42:59 -0600

    function for disabling button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+enableEnterButton();
+
 $('.enter').click(function() {
 	var titleInput = $('.title-input').val()
 	var urlInput = $('.url-input').val()
+	// enableEnterButton();
 	var cardTemplate = `<article class="card">
 			<h3 class="website-title website-title-1">  ${titleInput} </h3>
 			<p class="website-url website-url-1"><a href="${urlInput}">  ${urlInput}  </a></p>
@@ -21,3 +24,30 @@ $('.bookmark-list').on('click', '.delete', function(){
 $('.url-input').on('click', function(){
 	$(this).val('http://')
 })
+
+$('.url-input').on('keyup', function(){
+	// console.log($('.url-input').val())
+	var titleInput = $('.title-input').val()
+    var urlInput = $('.url-input').val()
+    if (titleInput !== "" && urlInput !== ""){
+        $('.enter').prop('disabled');
+        console.log("if")
+        } else {
+        // $('.enter').prop('disabled');
+        console.log('else')
+    }
+});
+
+
+function enableEnterButton() {
+	console.log('hi')
+    var titleInput = $('.title-input').val()
+    var urlInput = $('.url-input').val()
+    if (titleInput === "" && urlInput === ""){
+        $('.enter').removeAttr('disabled');
+        console.log("if")
+        } else {
+        $('.enter').setAttr('disabled');
+        console.log('else')
+    }
+}
