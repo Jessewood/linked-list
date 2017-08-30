7dc5bdca89a6d7119eafa05b7e432593a8a44111
Jesse Packwood <packwoodjustin@gmail.com>
2017-08-30 14:20:20 -0600

    enter button works properly

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 
 
 $('.enter').click(function() {
-	var titleInput = $('.title-input').val()
-	var urlInput = $('.url-input').val()
-	// enableEnterButton();
+	var titleInput = $('.title-input').val();
+	var urlInput = $('.url-input').val();
 	var cardTemplate = `<article class="card">
 			<h3 class="website-title website-title-1">  ${titleInput} </h3>
 			<p class="website-url website-url-1"><a href="${urlInput}">  ${urlInput}  </a></p>
 			<button class="read-style">Read</button><button class="delete">Delete</button>
 		</article>`
 	$('.bookmark-list').append(cardTemplate);
-	updateCardCount()
-	$('.bookmark-number').text($('.card').length)
+	updateCardCount();
+	$('.bookmark-number').text($('.card').length);
+	$('.title-input').val("");
+	$('.url-input').val("");
+	$('.enter').attr('disabled',true)
+
 });
 
 $('.bookmark-list').on('click', '.read-style', function(){
 	$(this).closest('article').toggleClass('read-card')
-})
+});
 
 $('.bookmark-list').on('click', '.delete', function(){
 	$(this).closest('article').remove();
@@ -27,14 +30,17 @@ $('.bookmark-list').on('click', '.delete', function(){
 function updateCardCount() {
 	var cardCount = $('.card').length;
 	console.log(cardCount)
-}
+};
 
 
 $('.url-input').on('click', function(){
 	$(this).val('http://')
-})
+});
 
-$('.url-input').on('keyup', function(){
+$('.url-input').on('input', enableEnter)
+$('.title-input').on('input', enableEnter)
+
+function enableEnter() {
 	var titleInput = $('.title-input').val()
     var urlInput = $('.url-input').val()
     if (titleInput !== "" && urlInput !== ""){
@@ -44,7 +50,7 @@ $('.url-input').on('keyup', function(){
         $('.enter').attr('disabled', true);
         console.log('else')
     }
-});
+};
 
 
 
